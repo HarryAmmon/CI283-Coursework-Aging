b40987f1fa676c1714fa46c57ed911fdca141d9d
Ammonation <harry_a99@hotmail.co.uk>
2019-04-11 11:38:07 +0100

    A process is now able to switch queue, closes #4

--- src/Process.java
+++ src/Process.java
@@ -5,6 +5,9 @@ public class Process implements Comparable<Process>{
 	private int executionTime;
 	private int waitTime;
 	private int priority;
+	private int originalQueue;
+	private int currentQueue;
+	private int intendedQueue;
 	
 	Process(String process){
 		String[] fields = process.split(",");
@@ -12,8 +15,10 @@ public class Process implements Comparable<Process>{
 		this.arrivalTime = Integer.parseInt(fields[1]);
 		this.burstTime = Integer.parseInt(fields[2]);
 		this.priority = Integer.parseInt(fields[3]);
-		this.executionTime = 0;
 		this.waitTime = 0;
+		this.originalQueue = calculateQueue();
+		this.currentQueue = -1;	// If the process has not yet been added a queue this will have the value of -1
+		this.intendedQueue = this.originalQueue;
 	}
 	
 	public int getPid() {
@@ -32,20 +37,44 @@ public class Process implements Comparable<Process>{
 		return this.priority;
 	}
 	
-	public void setPriority(int i) {
-		this.priority = i;
-	}
-	
 	public int getExecutionTime() {
 		return this.executionTime;
 	}
 	
-	public void age() {
+	public int getOriginalQueue() {
+		return this.originalQueue;
+	}
+	
+	public int getCurrentQueue() {
+		return this.currentQueue;
+	}
+	
+	public void setCurrentQueue(int i) {
+		this.currentQueue = i;
+	}
+	
+	public int getIntendedQueue() {
+		return this.intendedQueue;
+	}
+	
+	public boolean age() {
 		this.waitTime++;
 		if (this.waitTime % 20 == 0 && this.priority > 0) {
 			this.priority--;
-			//System.out.printf("PRIORITY DECREASED FOR PID: %4d%n", this.pid); //REMOVE
+			if(calculateQueue() != currentQueue) {
+				this.intendedQueue = calculateQueue();
+				return true;
+			}
+
 		}
+		return false;
+	}
+	
+	private int calculateQueue() {
+		if(this.priority <= 42) {return 1;}
+		else if(this.priority > 42 && this.priority <= 84) {return 2;}
+		else if(this.priority > 84) {return 3;}
+		else {return -1;}
 	}
 	@Override
 	public int compareTo(Process comparePr) {
@@ -53,9 +82,8 @@ public class Process implements Comparable<Process>{
 		return this.arrivalTime - compareArrivalTime;
 	}
 	
-	public void execute(int systemTime){
-		this.executionTime = systemTime;
-		System.out.printf("PID: %4d | ArrivalTime: %5d | ExecutionTime: %6d | WaitTime: %5d | BurstTime: %4d | Priority: %2d%n", this.pid,this.arrivalTime,this.executionTime,this.waitTime,this.burstTime,this.priority);
+	public void execute(){
+		System.out.printf("PID: %4d | ArrivalTime: %5d | WaitTime: %5d | BurstTime: %4d | Priority: %2d | OriginalQueue: %1d | CurrentQueue: %1d |%n", this.pid,this.arrivalTime,this.waitTime,this.burstTime,this.priority,this.originalQueue,this.currentQueue);
 	}
 	
 
